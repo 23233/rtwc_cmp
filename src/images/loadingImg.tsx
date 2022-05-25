d154d5a2a3f948fab21e5849d6d01f90239ff3b2
1 <1@qq.com>
2022-05-25 15:51:04 +0800

    feat: `Lmg` remain `ssr`

--- src/images/loadingImg.tsx
+++ src/images/loadingImg.tsx
@@ -83,31 +83,25 @@ const Lmg: React.FC<LmgProps> = ({
         />
       );
     }
-
-    // 浏览器
-    if (typeof window !== 'undefined') {
-      return (
-        <div className={loadingClassName} style={{ minHeight: loadingMinSize }}>
-          <svg className="animate-spin h-5 w-5 text-gray-200" viewBox="0 0 24 24">
-            <circle
-              className="opacity-20"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-80"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
-        </div>
-      );
-    }
-    // node环境 用于ssr
-    return <img src={src} alt={alt} title={alt} className={'hidden'} />;
+    return (
+      <div className={loadingClassName} style={{ minHeight: loadingMinSize }}>
+        <svg className="animate-spin h-5 w-5 text-gray-200" viewBox="0 0 24 24">
+          <circle
+            className="opacity-20"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          />
+          <path
+            className="opacity-80"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
+      </div>
+    );
   }, [props]);
 
   return <React.Fragment>{renderItem}</React.Fragment>;
