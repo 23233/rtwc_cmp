269e639f63421f0c380a18d0fb076b68c833a22e
1 <1@qq.com>
2022-05-30 12:34:25 +0800

    feat: `Images` 新增快捷多图模式

--- src/images/demo.tsx
+++ src/images/demo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Lmg, PreviewImgView, fastImgPreviewOpen} from '@rtwc/cmp';
+import { Lmg, PreviewImgView, fastImgPreviewOpen } from '@rtwc/cmp';
+import { fastImgsPreviewOpen } from './previews/open';
 
 const index: React.FC = () => {
   const img =
@@ -11,35 +12,43 @@ const index: React.FC = () => {
     fastImgPreviewOpen(src);
   };
 
+  const onManyPreviewClick = () => {
+    const imgList = [img, origin];
+    const nowIndex = 1;
+    fastImgsPreviewOpen(imgList, imgList[nowIndex]);
+  };
+
   return (
     <div className={'m-2'}>
       <div>普通图片加载</div>
 
-      <Lmg src={img} alt={'图片'}/>
+      <Lmg src={img} alt={'图片'} />
 
       <div className={'my-2'}>背景图片模式</div>
 
       <div className={'h-24'}>
-        <Lmg src={img} useBk/>
+        <Lmg src={img} useBk />
       </div>
 
       <div className={'my-2'}>背景图片设置size为contain</div>
 
       <div className={'h-24'}>
-        <Lmg src={img} useBk bgSize={'contain'}/>
+        <Lmg src={img} useBk bgSize={'contain'} />
       </div>
-
 
       <div className={'my-2'}>点击可放大预览</div>
 
       <div>
-        <PreviewImgView src={img} origin={origin}/>
+        <PreviewImgView src={img} origin={origin} />
       </div>
 
       <div className={'my-2'}>
         代码层调用放大预览
         <div>
           <button onClick={() => onPreviewCLick(img)}>点我预览图片</button>
+        </div>
+        <div>
+          <button onClick={onManyPreviewClick}>点我预览多张图</button>
         </div>
       </div>
     </div>
--- src/images/previews/open.tsx
+++ src/images/previews/open.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import Viewer from "viewerjs";
+import Viewer from 'viewerjs';
 import 'viewerjs/src/css/viewer.scss';
-
 
 // 通过dom打开
 export function fastImgPreviewOpen(src: string): any {
-
   const img = document.createElement('img');
   img.src = src;
   const viewer = new Viewer(img, {
     zIndex: 9999,
     hide(event: CustomEvent) {
       img.remove();
-    }
+    },
   });
   viewer.show();
+}
 
+export function fastImgsPreviewOpen(imgs: Array<string>, now?: string) {
+  const nowIndex = now ? imgs.findIndex((b) => b === now) || 0 : 0;
+  const warp = document.createElement('ul');
+  imgs.map((d) => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = d;
+    li.appendChild(img);
+    warp.appendChild(li);
+  });
+
+  const viewer = new Viewer(warp, {
+    zIndex: 9999,
+    initialViewIndex: nowIndex,
+    hide(event: CustomEvent) {
+      warp.remove();
+    },
+  });
+  viewer.show();
 }
