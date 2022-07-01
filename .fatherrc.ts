8b41bc40bb6ecdb8fb85ae7fb01b1ce2970e44d3
1 <1@qq.com>
2022-07-01 13:42:49 +0800

    去掉umd 升级各类依赖

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,14 +3,14 @@ import { IBundleOptions } from 'father-build/src/types';
 export default {
   esm: { type: 'babel', mjs: false, importLibToEs: true },
   cjs: { type: 'babel', lazy: true },
-  umd: {
-    name: 'rtwc-cmp',
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDom',
-      'react-router-dom': 'ReactRouterDom',
-    },
-  },
+  // 目前看来没有必要打包出umd esm cjs就足够了
+  // umd: {
+  //   name: 'rtwc-cmp',
+  //   minFile:true,
+  //   globals: {
+  //     react: 'React',
+  //   },
+  // },
   extractCSS: true,
   runtimeHelpers: true,
 } as IBundleOptions;
