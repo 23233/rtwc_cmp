16dfd932acc484eea66d43f0b916a35242097ff8
1 <1@qq.com>
2022-07-04 21:09:07 +0800

    更新文档

--- src/tools/demo.tsx
+++ src/tools/demo.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import {downloadFile, sendClipboard} from "@rtwc/cmp"
+import { downloadFile } from '@rtwc/cmp';
 
 export default () => {
-
   const testDownload = () => {
-    let b = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-    downloadFile(b, "hello world.txt")
-  }
+    let b = new Blob(['Hello, world!'], { type: 'text/plain;charset=utf-8' });
+    downloadFile(b, 'hello world.txt');
+  };
 
   return (
     <div>
-      <button onClick={() => sendClipboard("赵日天")}>方法调用写入剪切板</button>
-
       <button onClick={testDownload}>测试文件下载</button>
-
     </div>
   );
 };
