d8908fa8d2cb07f305cfe7855d7b48a9a2956b62
1 <1@qq.com>
2022-07-04 21:07:38 +0800

    sendClipboard 也移入 comm中

--- src/tools/file.ts
+++ src/tools/file.ts
@@ -1,17 +1,5 @@
-import {saveAs} from 'file-saver';
+import { saveAs } from 'file-saver';
 // 文件下载
 export const downloadFile = (url: string | Blob, filename: string): void => {
   saveAs(url, filename);
-};
-
-// 写入剪切板
-export const sendClipboard = (text: string, cb?: () => void): void => {
-  if (!text) return;
-  const textField = document.createElement('textarea');
-  textField.innerText = text;
-  document.body.appendChild(textField);
-  textField.select();
-  document.execCommand('copy');
-  textField.remove();
-  cb && cb();
 };
